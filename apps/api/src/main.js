a54beee10f25eb2ab7b7c4ae95a6dbef04185054
Ashukla011 <avnishkumar492862@gmail.com>
2026-09-17 09:50:33 +0530

    cors issue fix

--- apps/api/src/main.js
+++ apps/api/src/main.js
@@ -14,12 +14,8 @@ const app_module_1 = require('./app.module');
 async function bootstrap() {
   const app = await core_1.NestFactory.create(app_module_1.AppModule);
   app.use((0, helmet_1.default)());
-  
-  app.enableCors({
-    origin: process.env.FRONTEND_URL?.split(',').map((origin) =>
-      origin.trim(),
-    ) ?? ['http://localhost:5173'],
-  });
+
+  app.enableCors();
   app.useGlobalPipes(
     new common_1.ValidationPipe({
       whitelist: true,
